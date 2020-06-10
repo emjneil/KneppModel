47afee2580110f836f1ec0b02afc184c145a1e3e
Emily N <emjneil@gmail.com>
2020-06-10 15:41:13 +0100

    5Species

--- ABC_latestVersion.py
+++ ABC_latestVersion.py
@@ -36,8 +36,9 @@ def ecoNetwork(X, t, interaction_strength_chunk, rowContents_growth):
     return output_array
 
 
-# Define the number of simulations to try
+# Define the number of simulations to try. Bode et al. ran a million
 NUMBER_OF_SIMULATIONS = 5
+
 
 # ------ Generate the interaction pairs ------
 
@@ -47,6 +48,7 @@ interactionMatrix_csv = pd.read_csv('./parameterMatrix.csv', index_col=[0])
 species = list(interactionMatrix_csv.columns.values)
 # remember the shape of the csv array
 interaction_length = len(interactionMatrix_csv)
+
 
 # # --- make networkX visual of parameter matrix --
 # networkVisual = nx.Graph()
@@ -61,7 +63,7 @@ interaction_length = len(interactionMatrix_csv)
 
 
 # pull the original sign / number from csv
-original_sign = []
+original_sign=[]
 
 for outer_index, outer_species in enumerate(species):
     for inner_index, inner_species in enumerate(species):
@@ -70,24 +72,51 @@ for outer_index, outer_species in enumerate(species):
         original_sign.append(new_sign)
 # find the shape of original_sign
 shape = len(original_sign)
-
 # make lots of these arrays, keeping sign consistent with original_sign
 iterStrength_list = np.array(
     [[np.random.uniform(0, 1, (shape,)) * original_sign] for i in range(NUMBER_OF_SIMULATIONS)])
 iterStrength_list.shape = (NUMBER_OF_SIMULATIONS, interaction_length, interaction_length)
 
+
 # convert to multi-index so that species' headers/cols can be added
-names = ['x', 'y', 'z']
+names = ['runs', 'species', 'z']
+index = pd.MultiIndex.from_product([range(s) for s in iterStrength_list.shape], names=names)
+interaction_strength = pd.DataFrame({'iterStrength_list': iterStrength_list.flatten()}, index=index)['iterStrength_list']
+#  re-shape so each run in NUMBER_OF_SIMULATIONS is its own column
+interaction_strength = interaction_strength.unstack(level='z').swaplevel(0, 1).sort_index()
+interaction_strength.index = interaction_strength.index.swaplevel(0, 1)
+interaction_strength = interaction_strength.sort_values(by=['runs', 'species'], ascending=[True, True])
+# add headers/columns
+interaction_strength.columns = species
+interaction_strength.index = species * NUMBER_OF_SIMULATIONS
+
+# find the shape of original_sign
+shape = len(original_sign)
+# make lots of these arrays, keeping sign consistent with original_sign
+iterStrength_list = np.array(
+    [[np.random.uniform(0, 1, (shape,)) * original_sign] for i in range(NUMBER_OF_SIMULATIONS)])
+iterStrength_list.shape = (NUMBER_OF_SIMULATIONS, interaction_length, interaction_length)
+
+
+# convert to multi-index so that species' headers/cols can be added
+names = ['runs', 'y', 'z']
 index = pd.MultiIndex.from_product([range(s) for s in iterStrength_list.shape], names=names)
 interaction_strength = pd.DataFrame({'iterStrength_list': iterStrength_list.flatten()}, index=index)[
     'iterStrength_list']
 #  re-shape so each run in NUMBER_OF_SIMULATIONS is its own column
 interaction_strength = interaction_strength.unstack(level='z').swaplevel(0, 1).sort_index()
 interaction_strength.index = interaction_strength.index.swaplevel(0, 1)
-interaction_strength = interaction_strength.sort_values(by=['x', 'y'], ascending=[True, True])
+interaction_strength = interaction_strength.sort_values(by=['runs', 'y'], ascending=[True, True])
 # add headers/columns
-interaction_strength.index = species * NUMBER_OF_SIMULATIONS
 interaction_strength.columns = species
+interaction_strength.index = species * NUMBER_OF_SIMULATIONS
+
+# this makes interaction_strength a 3D array
+#   interaction_strength["species"] = species*NUMBER_OF_SIMULATIONS
+#   interaction_strength = interaction_strength.reset_index().set_index(["runs","species"])
+#   del interaction_strength['y']
+
+
 
 
 # # ------- Define the parameters to try in the first ABC ------
@@ -104,20 +133,19 @@ initial_numbers_csv = pd.read_csv('./initial_numbers.csv')
 X0 = pd.DataFrame(np.random.uniform(low=initial_numbers_csv.iloc[0],high=initial_numbers_csv.iloc[1], size=(NUMBER_OF_SIMULATIONS, interaction_length)),columns=initial_numbers_csv.columns)
 
 
-
 ###### --------- Solve the ODE-----------
 
-# Define time points: first 5 years, 2000-2004
-t = np.linspace(0, 5, 50)
+# Define time points: first 10 years, 2000-2009
+t = np.linspace(0, 10, 100)
 
 all_runs = []
 
+# all_parameters = pd.concat([X0, growthRates, interaction_strength])
+
 # loop through each row of data
-for (rowNumber, rowContents_X0), (rowNumber, rowContents_growth), (interaction_strength_chunk) in zip(X0.iterrows(),
-                                                                                                      growthRates.iterrows(),
-                                                                                                      np.array_split(
-                                                                                                              interaction_strength,
-                                                                                                              NUMBER_OF_SIMULATIONS)):
+for (rowNumber, rowContents_X0), (rowNumber, rowContents_growth), (interaction_strength_chunk) in zip(X0.iterrows(),growthRates.iterrows(),np.array_split(interaction_strength,NUMBER_OF_SIMULATIONS)):
+    # parameters_used = pd.concat([rowContents_X0, rowContents_growth, interaction_strength_chunk])
+    # all_parameters['id'] = list(range(NUMBER_OF_SIMULATIONS)*2)
     # print(rowContents_X0)
     # print(rowContents_growth)
     # print(interaction_strength_chunk)
@@ -125,24 +153,39 @@ for (rowNumber, rowContents_X0), (rowNumber, rowContents_growth), (interaction_s
     first_ABC = integrate.odeint(ecoNetwork, rowContents_X0, t, args=(interaction_strength_chunk, rowContents_growth))
     # append to list
     all_runs = np.append(all_runs, first_ABC)
+    # parameters_used.append(all_parameters, parameters_used)
 
-final_runs = pd.DataFrame(all_runs.reshape(NUMBER_OF_SIMULATIONS * 50, len(species)), columns=species)
-# print(final_runs)
+final_runs = pd.DataFrame(all_runs.reshape(NUMBER_OF_SIMULATIONS * 100, len(species)), columns=species)
+
+
+##### ------ Plotting Populations ---------
+herbivores, youngScrub, matureScrub, woodland, fox = first_ABC.T
+plt.plot(t, herbivores, label = 'Rabbit')
+plt.plot(t, youngScrub, label = 'Grassland')
+plt.plot(t, matureScrub, label = 'Tree Sapling')
+plt.plot(t, woodland, label = 'Woodland')
+plt.plot(t, fox, label = 'Fox')
+plt.legend(loc='upper right')
+plt.show()
+
 
 ###### --------- Filter out unrealistic runs -----------
 
-# assign conditions - e.g. make sure none of the nodes are below zero at time stamp ~5 yrs
+# # select only the last run (filter to the year 2009)
+# accepted_simulations = final_runs.iloc[99::100, :]
+# # assign conditions
+# cond1 = accepted_simulations["woodland"] < 20 & accepted_simulations["woodland"] > 14
+# cond2 = accepted_simulations["matureScrub"] < 10 & accepted_simulations["matureScrub"] > 1
+# # cond3 = accepted_simulations["matureScrub"] < 10 & accepted_simulations["matureScrub"] > 1
+
+# # make accepted_simulations only those that meet the conditions in 2009
+# accepted_simulations = accepted_simulations[cond1 & cond2]
+
+
+# pull row number of the accepted simulations
+# final_runs - add "accepted" and "rejected" to the ones that match accepted_simulations
+# accepted_parameters <- all_parameters, filter the row from accepted_simulations
+
 
 
 # # ###### --------- Take posteriors of runs and plug into next  -----------
-
-
-# # ##### ------ Plotting Populations ---------
-
-herbivores, youngScrub, matureScrub, woodland = first_ABC.T
-plt.plot(t, herbivores, label = 'Herbivores')
-plt.plot(t, youngScrub, label = 'Young scrub')
-plt.plot(t, matureScrub, label = 'Mature scrub')
-plt.plot(t, woodland, label = 'Woodland')
-plt.legend(loc='upper right')
-plt.show()
