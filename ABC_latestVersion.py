c0f43ff6c1a944a5b37f23c7b19315602abbf367
Emily N <emjneil@gmail.com>
2020-08-13 10:05:08 +0100

    minimization_finished

--- ABC_latestVersion.py
+++ ABC_latestVersion.py
@@ -22,7 +22,7 @@ import seaborn as sns
 start = timeit.default_timer()
 
 # define the number of simulations to try. Bode et al. ran a million
-NUMBER_OF_SIMULATIONS = 200
+NUMBER_OF_SIMULATIONS = 50
 
 def ecoNetwork(t, X, interaction_strength_chunk, rowContents_growth):
     # define new array to return
@@ -34,7 +34,7 @@ def ecoNetwork(t, X, interaction_strength_chunk, rowContents_growth):
         for inner_index, inner_species in enumerate(species):
             # grab one set of interaction matrices at a time
             amount += interaction_strength_chunk[outer_species][inner_species] * X[outer_index] * X[inner_index]
-            if amount.item() >= 5:
+            if amount >= 5:
                 amount = None
                 break
         # append values to output_array
@@ -59,18 +59,9 @@ def generateInteractionMatrix():
     species = list(interactionMatrix_csv.columns.values)
     # remember the shape of the csv array
     interaction_length = len(interactionMatrix_csv)
-    # pull the original sign of the interaction
-    original_sign=[]
-    for outer_index, outer_species in enumerate(species):
-        for inner_index, inner_species in enumerate(species):
-            # pull the sign
-            new_sign = interactionMatrix_csv[inner_species][outer_species]
-            original_sign.append(new_sign)
-    # find the shape of original_sign
-    shape = len(original_sign)
-    # make lots of these arrays, keeping sign consistent with original_sign
+    # make lots of these arrays, with the minimimum = minimization value/2; maximum = minimum*4 (minimization value * 2)
     iterStrength_list = np.array(
-        [[np.random.uniform(0, 1, (shape,)) * original_sign] for i in range(NUMBER_OF_SIMULATIONS)])
+        [[np.random.uniform(interactionMatrix_csv.values, interactionMatrix_csv.values*4)] for i in range(NUMBER_OF_SIMULATIONS)])
     iterStrength_list.shape = (NUMBER_OF_SIMULATIONS, interaction_length, interaction_length)
     # convert to multi-index so that species' headers/cols can be added
     names = ['runs', 'species', 'z']
@@ -85,11 +76,13 @@ def generateInteractionMatrix():
     interaction_strength.index = species * NUMBER_OF_SIMULATIONS
     return interaction_strength
 
+
 def generateGrowth():
     growthRates_csv = pd.read_csv('./growthRates.csv')
     # generate new dataframe with random uniform distribution between the values 
     growthRates = pd.DataFrame(np.random.uniform(low=growthRates_csv.iloc[0],high=growthRates_csv.iloc[1], size=(NUMBER_OF_SIMULATIONS, len(species))),columns=growthRates_csv.columns)
     return growthRates
+    
 
 def generateX0():
     initial_numbers_csv = pd.read_csv('./initial_numbers.csv')
@@ -123,7 +116,7 @@ def generateX0():
 
 def runODE_1():
     # Define time points: first 10 years (2000-2009)
-    t = np.linspace(0, 10, 100)
+    t = np.linspace(0, 10, 50)
     all_runs = []
     all_parameters = []
     X0 = generateX0()
@@ -141,7 +134,7 @@ def runODE_1():
         # append all the parameters
         all_parameters.append(parameters_used)
     # check the final runs
-    final_runs = (np.vstack(np.hsplit(all_runs.reshape(len(species)*NUMBER_OF_SIMULATIONS, 100).transpose(),NUMBER_OF_SIMULATIONS)))
+    final_runs = (np.vstack(np.hsplit(all_runs.reshape(len(species)*NUMBER_OF_SIMULATIONS, 50).transpose(),NUMBER_OF_SIMULATIONS)))
     final_runs = pd.DataFrame(data=final_runs, columns=species)
     # append all the parameters to a dataframe
     all_parameters = pd.concat(all_parameters)
@@ -150,14 +143,16 @@ def runODE_1():
     return final_runs,all_parameters, X0
 
 
+
 # --------- FILTER OUT UNREALISTIC RUNS -----------
+
 def filterRuns_1():
     final_runs,all_parameters,X0 = runODE_1()
     # add ID to dataframe
     IDs = np.arange(1,1 + NUMBER_OF_SIMULATIONS)
-    final_runs['ID'] = np.repeat(IDs,100)
+    final_runs['ID'] = np.repeat(IDs,50)
     # select only the last run (filter to the year 2009)
-    accepted_year = final_runs.iloc[99::100, :]
+    accepted_year = final_runs.iloc[49::50, :]
     for_printing = accepted_year.dropna()
     # print that
     with pd.option_context('display.max_columns', None):
@@ -165,11 +160,9 @@ def filterRuns_1():
         print(for_printing.shape)
     # add filtering criteria
     accepted_simulations = accepted_year[(accepted_year['roeDeer'] <= (X0['roeDeer'].max()*4)) & (accepted_year['roeDeer'] >= (X0['roeDeer'].min()))
-    # (accepted_year['fox'] <= (X0['fox'].max())) & (accepted_year['fox'] >= (X0['fox'].min())) &
-    # (accepted_year['rabbits'] <= (X0['rabbits'].max())) & (accepted_year['rabbits'] >= (X0['rabbits'].min())) &
-    # (accepted_year['arableGrass'] <= (X0['arableGrass'].max())) & (accepted_year['arableGrass'] >= (X0['arableGrass'].min()*0.79))
-    # (accepted_year['woodland'] <= (X0['woodland'].max()*1.57)) & (accepted_year['woodland'] >= (X0['woodland'].min()*2.4))
-    # (accepted_year['thornyScrub'] <= (X0['thornyScrub'].max()*2)) & (accepted_year['thornyScrub'] >= (X0['thornyScrub'].min()))
+    (accepted_year['arableGrass'] <= (X0['arableGrass'].max())) & (accepted_year['arableGrass'] >= (X0['arableGrass'].min()*0.79))
+    (accepted_year['woodland'] <= (X0['woodland'].max()*1.57)) & (accepted_year['woodland'] >= (X0['woodland'].min()*2.4))
+    (accepted_year['thornyScrub'] <= (X0['thornyScrub'].max()*2)) & (accepted_year['thornyScrub'] >= (X0['thornyScrub'].min()))
     ]
     print(accepted_simulations.shape)
     # match ID number in accepted_simulations to its parameters in all_parameters
@@ -177,6 +170,7 @@ def filterRuns_1():
     # add accepted ID to original dataframe
     final_runs['accepted?'] = np.where(final_runs['ID'].isin(accepted_parameters['ID']), 'Yes', 'No')
     return accepted_parameters, accepted_simulations, final_runs
+
 
 
 
@@ -194,12 +188,14 @@ def generateParameters2():
     X0_2 = accepted_parameters.loc[accepted_parameters['X0'].notnull(), ['X0']]
     X0_2 = pd.DataFrame(X0_2.values.reshape(len(accepted_simulations), len(species)), columns = species)
     # # add reintroduced species & divide by 200 to keep the scaling/normalization consistent
-    X0_2.loc[:, 'largeHerb'] = [np.random.uniform(low=38.25, high=77.68)/200 for i in X0_2.index]
-    X0_2.loc[:,'tamworthPig'] = [np.random.uniform(low=6.36, high=11.77)/200 for i in X0_2.index]
+    X0_2.loc[:, 'largeHerb'] = [np.random.uniform(low=9671, high=18820)/200 for i in X0_2.index]
+    X0_2.loc[:,'tamworthPig'] = [np.random.uniform(low=1573, high=2910)/200 for i in X0_2.index]
     # # select interaction matrices part of the dataframes 
     interaction_strength_2 = accepted_parameters.drop(['X0', 'growth', 'ID'], axis=1)
     interaction_strength_2 = interaction_strength_2.dropna()
     return growthRates_2, X0_2, interaction_strength_2, accepted_simulations, accepted_parameters, final_runs
+
+
 
 
 # # # # # ------ SOLVE ODE #2: Pre-reintroductions (2009-2010) -------
@@ -208,7 +204,7 @@ def runODE_2():
     growthRates_2, X0_2, interaction_strength_2, accepted_simulations, accepted_parameters, final_runs  = generateParameters2()
     all_runs_2 = []
     all_parameters_2 = []
-    t_2 = np.linspace(0, 1, 10)
+    t_2 = np.linspace(0, 1, 5)
     # loop through each row of data
     for (rowNumber, rowContents_X0), (rowNumber, rowContents_growth), (interaction_strength_chunk) in zip(X0_2.iterrows(),growthRates_2.iterrows(),np.array_split(interaction_strength_2,len(accepted_simulations))):
         # concantenate the parameters
@@ -221,19 +217,21 @@ def runODE_2():
         # append all the parameters
         all_parameters_2.append(parameters_used_2)
     # check the final runs
-    final_runs_2 = (np.vstack(np.hsplit(all_runs_2.reshape(len(species)*len(accepted_simulations), 10).transpose(),len(accepted_simulations))))
+    final_runs_2 = (np.vstack(np.hsplit(all_runs_2.reshape(len(species)*len(accepted_simulations), 5).transpose(),len(accepted_simulations))))
     final_runs_2 = pd.DataFrame(data=final_runs_2, columns=species)
     # add ID to the dataframe
     IDs = np.arange(1,1 + len(accepted_simulations))
-    final_runs_2['ID'] = np.repeat(IDs,10)
+    final_runs_2['ID'] = np.repeat(IDs,5)
     return final_runs_2, all_parameters_2, X0_2, accepted_simulations, accepted_parameters, final_runs
+
+
 
 
 # --------- FILTER OUT UNREALISTIC RUNS: Post-reintroductions -----------
 def filterRuns_2():
     final_runs_2, all_parameters_2, X0_2, accepted_simulations, accepted_parameters, final_runs = runODE_2()
     # select only the last run (filter to the year 2010)
-    accepted_year_2010 = final_runs_2.iloc[9::10, :]
+    accepted_year_2010 = final_runs_2.iloc[4::5, :]
     accepted_simulations_2010 = accepted_year_2010[(accepted_year_2010['roeDeer'] <= (X0_2['roeDeer'].max())) & (accepted_year_2010['roeDeer'] >= (X0_2['roeDeer'].min()))
     # # (accepted_year_2010['fox'] <= (X0_2['fox'].max()*100)) & (accepted_year_2010['fox'] >= (X0_2['fox'].min()*.0001))
     # # (accepted_year_2010['rabbits'] <= (X0_2['rabbits'].max())) & (accepted_year_2010['rabbits'] >= (X0_2['rabbits'].min())) &
@@ -250,7 +248,9 @@ def filterRuns_2():
 
 
 
-# # # # # ----------------------------- PLOTTING POPULATIONS  (2000-2010) ----------------------------- 
+
+
+# # # # # ----------------------------- PLOTTING POPULATIONS (2000-2010) ----------------------------- 
 
 def plotting():
     accepted_simulations_2010, accepted_parameters_2010, final_runs_2, all_parameters_2, X0_2, accepted_simulations, accepted_parameters, final_runs = filterRuns_2()
@@ -267,21 +267,21 @@ def plotting():
     # concatenate them
     y_values = np.concatenate((final_runs1, final_runs2), axis=None)
     # we want species column to be spec1,spec2,spec3,spec4, etc.
-    species_firstRun = np.tile(species, 100*NUMBER_OF_SIMULATIONS)
-    species_secondRun = np.tile(species, 10*len(accepted_simulations))
+    species_firstRun = np.tile(species, 50*NUMBER_OF_SIMULATIONS)
+    species_secondRun = np.tile(species, 5*len(accepted_simulations))
     species_list = np.concatenate((species_firstRun, species_secondRun), axis=None)
     # add a grouping variable to graph each run separately
     grouping1 = np.arange(1,NUMBER_OF_SIMULATIONS+1)
-    grouping_variable1 = np.repeat(grouping1,100*len(species))
+    grouping_variable1 = np.repeat(grouping1,50*len(species))
     grouping2 = np.arange(NUMBER_OF_SIMULATIONS+2, NUMBER_OF_SIMULATIONS + 2 + len(accepted_simulations))
-    grouping_variable2 = np.repeat(grouping2,10*len(species))
+    grouping_variable2 = np.repeat(grouping2,5*len(species))
     # concantenate them 
     grouping_variable = np.concatenate((grouping_variable1, grouping_variable2), axis=None)
     # years - we've looked at 11 so far (2000-2010)
     year = np.arange(1,11)
-    indices1 = np.repeat(year,len(species)*10)
+    indices1 = np.repeat(year,len(species)*5)
     indices1 = np.tile(indices1, NUMBER_OF_SIMULATIONS)
-    indices2 = np.repeat(11,len(species)*10)
+    indices2 = np.repeat(11,len(species)*5)
     indices2 = np.tile(indices2, len(accepted_simulations))
     indices = np.concatenate((indices1, indices2), axis=None)
     # put it in a dataframe
--- minimization.py
+++ minimization.py
@@ -41,58 +41,74 @@ def ecoNetwork(t, X, interaction_strength_chunk, rowContents_growth):
     # arableGrass start out 338 to 401; filter is to 267 to 401; average = 334
     # woodland start out 22.3 to 62.3; filter is to 53.4 to 97.9; average = 75.7
     # thornyScrub start out 4.5 to 44.5; filter is 4.9 to 89; average = 46.95
+all_iterations_outputs = []
+allIterationsResults = []
 
 def objectiveFunction(x):
-    X0 = x[0:9]
-    growthRate = x[9:18]
-    rowContents_growth = pd.DataFrame(data=growthRate.reshape(1,9),columns=species)
-    interaction_strength = x[18:99]
-    interaction_strength_chunk = pd.DataFrame(data=interaction_strength.reshape(9,9),index = species, columns=species)
+    X0 = x[0:13] 
+    growthRate = x[13:26]
+    rowContents_growth = pd.DataFrame(data=growthRate.reshape(1,13),columns=species)
+    interaction_strength = x[26:195]
+    interaction_strength_chunk = pd.DataFrame(data=interaction_strength.reshape(13,13),index = species, columns=species)
     t = np.linspace(0, 10, 50)
     results = solve_ivp(ecoNetwork, (0, 10), X0,  t_eval = t, args=(interaction_strength_chunk, rowContents_growth), method = 'LSODA')
     # reshape the outputs
     y = (np.vstack(np.hsplit(results.y.reshape(len(species), 50).transpose(),1)))
-    # choose the final year (we want to compare the final year to the filters) & make sure the filters are also normalized (/200)
+    # choose the final year (we want to compare the final year to the middle of the filters) & make sure the filters are also normalized (/200)
+    outputs = (y[49:50:,])
     print(y[49:50:,])
-    print (((y[49:50:, 2]-(226/200))**2) + ((y[49:50:, 6]-(334/200))**2) + ((y[49:50:, 7]-(75.7/200))**2) + ((y[49:50:, 8]-(46.95/200))**2))
-    return (((y[49:50:, 2]-(226/200))**2) + ((y[49:50:, 6]-(334/200))**2) + ((y[49:50:, 7]-(75.7/200))**2) + ((y[49:50:, 8]-(46.95/200))**2))
+    result = (((y[49:50:, 1]-(226/200))**2) + ((y[49:50:, 5]-(49.885/200))**2) + ((y[49:50:, 7]-(4.32/200))**2) + ((y[49:50:, 9]-(334/200))**2) + ((y[49:50:, 10]-(75.7/200))**2) + ((y[49:50:, 11]-(46.95/200))**2))
+    all_iterations_outputs.append(outputs)
+    allIterationsResults.append(result)
+    print(result)
+    return result
 
 # here are my guesses to start at
-X0guess_init = [0,0,50,0,0,0,350,40,20]
-
+X0guess_init = [0,42,0,0,208,52,12,4,20,346,48,36,17]
 # normalize X0
 X0guess = [x / 200 for x in X0guess_init]
-growthGuess = [0,0,0.5,0,0,0,5,5,5]
-interactionGuess = [0,0,0,0,0,0,-0.1,-0.1,-0.1,
-                    0,0,0,0,0,0,-0.1,-0.1,-0.1,
-                    0,0,0,0,0,0,-0.5,-0.2,-0.3,
-                    0,0,0,0,0,0,-0.1,-0.1,-0.1,
-                    0,0,0,0,0,0,-0.1,-0.1,-0.1,
-                    0,0,0,0,0,0,-0.1,-0.1,-0.1,
-                    0.3,0.2,0.5,1,1,1,0,0,0,
-                    0.1,0.2,0.3,1,1,1,-0.3,0,-0.2,
-                    0.2,0.1,0.3,1,1,1,-0.2,0.3,0]
+growthGuess = [0,0.38,0,0,0.47,0.65,0.1,0.1,0.1,0.94,0.10,0.38,0]
+interactionGuess = [0,0,0,0,0,0,0,0,0,-0.056,-0.056,-0.056,0,
+                    0,0,0,0,0,0.305,0,0,0,-0.18,-0.055,-0.055,0,
+                    0,0,0,0,0,0,0,0,-0.055,-0.34,-0.056,-0.056,0,
+                    0,0,0,0,0,0,0,0,0,-0.57,-0.38,0,0.94,
+                    0,0,0,0,0,0.056,0,0.94,0,-0.23,-0.056,-0.13,0,
+                    0,-0.055,0,0,-0.58,0,0,0,0,0,0,0,0,
+                    0,0,0,0,0,0,0,0.944,0,0,0,0,0,
+                    0,0,0,0,-0.056,0,-0.056,0,-0.056,0,0,0,0,
+                    0,0,0.93,0,0,0,0,0.93,0,0,0,0,0,
+                    0.93,0.93,0.93,0.93,0.93,0,0,0,0,0,0,0,0,
+                    0.93,0.93,0.93,0.93,0.93,0,0,0,0,-0.84,0,-0.53,0,
+                    0.93,0.93,0.93,0,0.38,0,0.93,0,0.93,-0.95,0.866,0,0,
+                    0.93,0.93,0.93,0.93,0,0,0.93,0.93,0.93,-0.056,-0.056,-0.056,0
+]
+
 combined = X0guess + growthGuess + interactionGuess
 guess = np.array(combined)
 
 # define bounds for each variable; make sure X0 bounds are normalized
-X0bds = ((0,0),(0,0),(2.25/200,112.46/200),(0,0),(0,0),(0,0),(338/200,401/200),(22.3/200,62.3/200),(4.5/200,44.5/200))
-growthbds = ((0,0),(0,0),(0,1),(0,0),(0,0),(0,0),(0,5),(0,5),(0,5))
-interactionbds = ((0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(-1,0),(-1,0),(-1,0),
-                    (0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(-1,0),(-1,0),(-1,0),
-                    (0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(-1,0),(-1,0),(-1,0),
-                    (0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(-1,0),(-1,0),(-1,0),
-                    (0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(-1,0),(-1,0),(-1,0),
-                    (0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(-1,0),(-1,0),(-1,0),
-                    (0,1),(0,1),(0,1),(0,1),(0,1),(0,1),(0,0),(0,0),(0,0),
-                    (0,1),(0,1),(0,1),(0,1),(0,1),(0,1),(-1,0),(0,0),(-1,0),
-                    (0,1),(0,1),(0,1),(0,1),(0,1),(0,1),(-1,0),(0,1),(0,0))
+X0bds = ((0,0),(2.25/200,112.46/200),(0,0),(0,0),(2.7/200,452/200),(1.12/200,97.53/200),(0.15/200,27/200),(0.11/200,8.42/200),(0.311/200,40.4/200),(338/200,401/200),(22.3/200,62.3/200),(4.5/200,44.5/200),(15/200,24/200))
+growthbds = ((0,0),(0,1),(0,0),(0,0),(0,1),(0,1),(0,1),(0,1),(0,1),(0,1),(0,1),(0,1),(0,0))
+interactionbds = ((0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(-1,0),(-1,0),(-1,0),(0,0),
+                    (0,0),(0,0),(0,0),(0,0),(0,0),(0,1),(0,0),(0,0),(0,0),(-1,0),(-1,0),(-1,0),(0,0),
+                    (0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(-1,0),(-1,0),(-1,0),(-1,0),(0,0),
+                    (0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(-1,0),(-1,0), (0,0),(0,1),
+                    (0,0),(0,0),(0,0),(0,0),(0,0),(0,1),(0,0),(0,1),(0,0),(-1,0),(-1,0),(-1,0),(0,0),
+                    (0,0),(-1,0),(0,0),(0,0),(-1,0),(0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(0,0),
+                    (0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(0,1),(0,0),(0,0),(0,0),(0,0),(0,0),
+                    (0,0),(0,0),(0,0),(0,0),(-1,0),(0,0),(-1,0),(0,0),(-1,0),(0,0),(0,0),(0,0),(0,0),
+                    (0,0),(0,0),(0,1),(0,0),(0,0),(0,0),(0,0),(0,1),(0,0),(0,0),(0,0),(0,0),(0,0),
+                    (0,1),(0,1),(0,1),(0,1),(0,1),(0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(0,0),(0,0),
+                    (0,1),(0,1),(0,1),(0,1),(0,1),(0,0),(0,0),(0,0),(0,0),(-1,0),(0,0),(-1,0),(0,0),
+                    (0,1),(0,1),(0,1),(0,0),(0,1),(0,0),(0,1),(0,0),(0,1),(-1,0),(0,1),(0,0),(0,0),
+                    (0,1),(0,1),(0,1),(0,1),(0,0),(0,0),(0,1),(0,1),(0,1),(-1,0),(-1,0),(-1,0),(0,0))
+
+
 # combine them into one dataframe
 bds = X0bds + growthbds + interactionbds
 
-
-# minimize the distance between the final year outputs & the filters (bounds)
-# optimization = optimize.minimize(objectiveFunction, x0 = guess, bounds = bds, method = 'L-BFGS-B')
-optimization = optimize.differential_evolution(objectiveFunction, bounds = bds)
+optimization = optimize.minimize(objectiveFunction, x0 = guess, bounds = bds, method = 'Powell', options ={'maxiter': 100}, tol=0.1)
+# optimization = optimize.differential_evolution(objectiveFunction, bounds = bds, maxiter = 1, tol=10000)
 print(optimization)
-
+# print(all_iterations_outputs)
+# print(allIterationsResults)
